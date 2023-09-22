28c8b654df1539d280d863f3afaae41e84f9eeab
Joel Jaison <joeljaison394@gmail.com>
2023-09-22 12:53:59 +0530

    Conditionas and contidional rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     import Binding from '../lib/components/binding.svelte';
     import '../app.postcss';
+	import Conditions from '../lib/components/conditions.svelte';
 </script>
 
 <body>
     <div class="container p-5">
         <Binding/>
+        <Conditions/>
     </div>
 
 </body>
